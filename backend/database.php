08da93c2df59467513a3aa8fad961a800d513351
Throrvik <thomas.rorvik@gmail.com>
2025-06-22 18:35:31 +0200

    Remove unnecessary CORS headers

--- backend/database.php
+++ backend/database.php
@@ -1,9 +1,4 @@
 <?php
-
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST");
-header("Access-Control-Allow-Headers: Content-Type");
-
 // Load credentials
 $config = require __DIR__ . '/config.php';
 
