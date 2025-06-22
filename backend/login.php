e272bc236a30006640a44f20cb7a5f4d7a588310
Throrvik <thomas.rorvik@gmail.com>
2025-06-22 23:01:24 +0200

    Add conditional debug mode to login

--- backend/login.php
+++ backend/login.php
@@ -1,7 +1,15 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// Enable verbose error output only in development mode
+$devMode = getenv('DEV_MODE') === 'true';
+if ($devMode) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+    error_reporting(0);
+}
 session_start();
 require_once 'database.php';
 
