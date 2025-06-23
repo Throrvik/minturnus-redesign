14c450323eba370c61cab5a5ed0442ada3c20f51
Throrvik <thomas.rorvik@gmail.com>
2025-06-23 10:59:34 +0200

    Add shared color constants module

--- js/user_card.js
+++ js/user_card.js
@@ -1,35 +1,3 @@
-const colorNames = {
-    "#FF6666": "Lys rød",
-    "#FFB266": "Lys oransje",
-    "#FFFF66": "Lys gul",
-    "#B2FF66": "Lime",
-    "#66FFB2": "Mint",
-    "#66B2FF": "Himmelblå",
-    "#CC66FF": "Fiolett",
-    "#FF66B2": "Knallrosa",
-    "#66FF66": "Klar grønn",
-    "#CCCCCC": "Grå",
-    "#FF8C00": "Mørk oransje",
-    "#FFD700": "Gull",
-    "#7CFC00": "Plengrønn",
-    "#40E0D0": "Turkis",
-    "#1E90FF": "Dodgerblå",
-    "#BA55D3": "Orkide",
-    "#FF1493": "Dyp rosa",
-    "#32CD32": "Limegrønn",
-    "#D3D3D3": "Lys grå",
-    "#8B0000": "Mørk rød",
-    "#A52A2A": "Brun",
-    "#2E8B57": "Sjøgrønn",
-    "#4682B4": "Stålblå",
-    "#FF4500": "Oransjerød",
-    "#DA70D6": "Orkide lilla",
-    "#B0C4DE": "Lys stålblå",
-    "#8A2BE2": "Blålilla",
-    "#20B2AA": "Lys sjøgrønn",
-    "#FF6347": "Tomatrød",
-    "#9ACD32": "Gulgrønn"
-};
 
 function createCard(user, options = {}) {
     const card = document.createElement('div');
